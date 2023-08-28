237c9de69c7e2c4367c39de137306a4295ce8ac2
andro-bulj <bulj.andro@gmail.com>
2023-08-28 17:14:01 +0200

    Change back title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -243,7 +243,7 @@ const App = () => {
     if (register) {
       return (
         <div className="login">
-          <h1 className="title">Budget visualiser</h1>
+          <h1 className="title">Budget Visualiser</h1>
           <RegisterForm
             name={name}
             username={username}
