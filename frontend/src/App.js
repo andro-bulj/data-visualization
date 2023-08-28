5776d1b20b6f5c59630a71decee3c7fdd277f7ea
andro-bulj <bulj.andro@gmail.com>
2023-08-28 17:12:26 +0200

    Update title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -243,7 +243,7 @@ const App = () => {
     if (register) {
       return (
         <div className="login">
-          <h1 className="title">Budget Visualiser</h1>
+          <h1 className="title">Budget visualiser</h1>
           <RegisterForm
             name={name}
             username={username}
@@ -259,7 +259,7 @@ const App = () => {
     } else {
       return (
         <div className="login">
-          <h1 className="title">Budget Visualiser</h1>
+          <h1 className="title">Budget visualiser</h1>
           <LoginForm
             username={username}
             password={password}
